12a68f598e15f4574fb19b5c37ba6836014c3b90
John Vazquez <vazquezjohn279s@gmail.com>
2025-11-12 22:09:47 -0600

    button is working

--- js/script.js
+++ js/script.js
@@ -1,7 +1,18 @@
 function init(){
+  var alert = document.getElementById("entrybutton").addEventListener("click", myFunction);
+  //object variable that point to the red text box
+}
 //add your javascrip between these two lines of code
  
 
+function myFunction() {
+  var txt = document.getElementById("entryinput").value;
+  //Grabs the text that is inputed
+  document.getElementById("textoutput").innerHTML = txt;
+  //Change the text after the button click
+
+  alert("John: " + txt);
+}
 
 
 
