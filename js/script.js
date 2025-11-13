c460f3bd3317aaeca7ac08269a0d226a9cee270b
John Vazquez <vazquezjohn279s@gmail.com>
2025-11-12 22:38:05 -0600

    changed functiones names

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 function init(){
-  var alert = document.getElementById("entrybutton").addEventListener("click", myFunction);
+  var alert = document.getElementById("entrybutton").addEventListener("click", alertFunction);
   //object variable that point to the red text box
 }
 //add your javascrip between these two lines of code
  
 
-function myFunction() {
+function alertFunction() {
   var txt = document.getElementById("entryinput").value;
   //Grabs the text that is inputed
   document.getElementById("textoutput").innerHTML = txt;
   //Change the text after the button click
 
-  alert("John: " + txt);
+  alert("John Vazquez: " + txt);
 }
 
 
